da760287f08d73b4a40539fafa8c59490d762934
deva-271205 <devaraj2712dec@gmail.com>
2026-04-03 00:44:57 +0530

    fix: add calculator.py - closes #1

---
+++ calculator.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+print("Calculator ready!")
+print("2 + 3 =", add(2, 3))
+print("5 - 2 =", subtract(5, 2))
+print("4 x 3 =", multiply(4, 3))
